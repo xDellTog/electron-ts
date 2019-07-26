7909a625abf5a5c9645bd1c158fc42548f5ded27
Daniel Tognon <xone.tognon@outlook.com>
2019-07-26 12:13:28 -0300

    Test Printer / Socket Java

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,5 +1,45 @@
-const btnAction = document.getElementById("btnAction");
+let socket: WebSocket;
 
-btnAction.addEventListener("click", () => {
-    console.log("Action");
+const btnConnect = document.getElementById("btnConnect");
+const btnDesconnect = document.getElementById("btnDesconnect");
+const btnPrint = document.getElementById("btnPrint");
+
+btnConnect.addEventListener("click", () => {
+    createSocket();
 });
+
+btnDesconnect.addEventListener("click", () => {
+    destroySocket();
+});
+
+btnPrint.addEventListener("click", () => {
+    socket.send(JSON.stringify({
+        action: "print",
+        data: {
+            fileName: "C:\\Users\\LTCloud\\Documents\\java\\Printer\\test.txt",
+            printerName: "Microsoft Print to PDF",
+        },
+    }));
+});
+
+function createSocket() {
+    socket = new WebSocket("ws://localhost:8080");
+    socket.onopen = () => {
+        console.log("On open...");
+    };
+    socket.onerror = () => {
+        console.log("On error...");
+    };
+    socket.onmessage = (evt) => {
+        console.log("On message...");
+        console.log(JSON.parse(evt.data));
+    };
+    socket.onclose = () => {
+        console.log("On close...");
+    };
+}
+
+function destroySocket() {
+    socket.close();
+    socket = null;
+}
